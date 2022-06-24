f4a6969e86edaafe8a2a7a9118d3d62be0c2ea35
Parker Parham <parkerparham@gmail.com>
2022-06-23 23:14:13 -0400

    commit tree dc6697300bca3947c46db5a91ec3c2a8767bf0a2

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(c, d) {
+    return c - d;
+}
+function multiply(e, f) {
+    return e * f;
+}
+function divide(g, h) {
+    return g / h;
+}
+function increment(a) {
+    return a += 1;
+}
+function decrement (a) {
+    return a -= 1;
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
